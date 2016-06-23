49000e901a4780b53d4fe5723216634dab7e7324
Jacob Peddicord <jacob@peddicord.net>
2016-06-23 00:54:56 -0700

    Playing around with bootstrap colors
    This isn't exactly pretty, but it could be
    
    Or I'm just not a designer, probably that

--- web/shared/components/SolutionUploader.jsx
+++ web/shared/components/SolutionUploader.jsx
@@ -9,13 +9,13 @@ export default class SolutionUploader extends React.Component {
   state = {
     ready: false,
     selected: null,
-    selectedSize: 0,
     error: null,
   };
 
   handleDrop = e => {
     e.stopPropagation();
     e.preventDefault();
+    this.setState({dragging: false});
     this.handleFiles(e.dataTransfer.files);
   };
 
@@ -53,21 +53,25 @@ export default class SolutionUploader extends React.Component {
     if (this.state.ready) {
       onSubmit(this.fileContent);
     }
-    this.setState({ready: false, error: null});
+    this.setState({ready: false, error: null, selected: null});
   };
 
   render() {
+    // TODO: this is a bit ugly
+    const colorClass = this.state.ready ? 'card-info card-inverse' : 'card-info';
     return (
-      <div>
-        <div style={{width: '200px', height: '100px', backgroundColor: '#cccccc', textAlign: 'center'}}
-          onDragEnter={e => { e.stopPropagation(); e.preventDefault(); }}
-          onDragOver={e => { e.stopPropagation(); e.preventDefault(); }}
-          onClick={e => { this.pickerRef.click(); }}
-          onDrop={this.handleDrop}>
-          {this.state.selected != null ? this.state.selected : 'drop something here or click to pick'}
-        </div>
-        <input type="file" style={{display: 'none'}} name="picker" onChange={this.handlePicker} ref={ref => this.pickerRef = ref} />
-        <button type="button" onClick={this.handleSubmit}>Upload</button>
+      <div className={`card card-block ${colorClass}`}
+        onDragEnter={e => { e.stopPropagation(); e.preventDefault(); }}
+        onDragOver={e => { e.stopPropagation(); e.preventDefault(); }}
+        onDrop={this.handleDrop}>
+
+          <p className="card-text">
+            {this.state.selected != null ? this.state.selected : 'Drag & drop a file here or pick one manually'}
+          </p>
+          <input type="file" style={{display: 'none'}} name="picker" onChange={this.handlePicker} ref={ref => this.pickerRef = ref} />
+          <button className="btn btn-link" type="button" onClick={e => this.pickerRef.click()}>Pick File</button>
+          <button className="btn btn-warning" type="button" onClick={this.handleSubmit} disabled={!this.state.ready}>Lock &amp; Submit</button>
+
       </div>
     );
   }
