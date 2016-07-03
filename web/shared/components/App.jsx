b50c6f68ee23a125ca4b828edba31dc3d5bb086b
Jacob Peddicord <jacob@peddicord.net>
2016-07-03 00:23:01 -0700

    Add a logout link

--- web/shared/components/App.jsx
+++ web/shared/components/App.jsx
@@ -6,12 +6,24 @@
  */
 
 /* global __BUILD */
+import alertify from 'alertify.js';
 import React from 'react';
 import StatusNav from '../containers/StatusNav';
+import { clearToken } from '../util/token';
 
 export default class App extends React.Component {
   static propTypes = {
     children: React.PropTypes.element,
+  };
+
+  logout = async e => {
+    const result = await alertify.okBtn('Log out').cancelBtn('Cancel')
+        .confirm('Sign out? You will need a recovery code to sign back in.');
+    result.event.preventDefault();
+    if (result.buttonClicked === 'ok') {
+      clearToken();
+      window.location.reload();
+    }
   };
 
   render() {
@@ -33,6 +45,11 @@ export default class App extends React.Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12">
+                <div className="pull-xs-right">
+                  <small>
+                    <button className="btn btn-link btn-sm" onClick={this.logout}>logout</button>
+                  </small>
+                </div>
                 <small className="text-muted">
                   brought to you by <a href="https://github.com/jpeddicord/progcon">progcon v{__BUILD.version}</a> built on {__BUILD.timestamp}
                 </small>
